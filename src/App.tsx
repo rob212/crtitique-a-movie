1e8b2aff2752bf711cc8df5c580182b06c8d51be
rob mcbryde <mcbrygro@amazon.com>
2025-09-08 14:47:14 +0100

    init commit, basic app skeleton

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me 2</Button>
+    </HStack>
+  )
+}
+
+export default App
